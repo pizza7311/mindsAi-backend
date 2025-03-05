6b44d9c7d01a153a9bc454b32d36d6b5bb453dab
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2025-03-05 21:10:38 +0900

    feat: Add create user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Body,
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'prisma/prisma.service';
+import { hashPassword } from 'utils/hash';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  async create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<string> {
+    const { email, name, password } = createUserDto;
+    const hashedPassword = await hashPassword(password);
+    try {
+      const res = await this.prisma.user.create({
+        data: {
+          email,
+          name,
+          password: hashedPassword,
+        },
+      });
+      return res.id;
+    } catch (e) {
+      //uk error
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Email already in use.');
+        }
+      }
+      throw new InternalServerErrorException(e);
+    }
   }
 
   async findAll() {
