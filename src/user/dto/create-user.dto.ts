2d50bf3da615062517602bf19535b7a1c242c24a
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2025-03-06 00:35:21 +0900

    docs: Add user dto swagger doc

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'test@test.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'test1234',
+  })
+  @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: 'user name',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,18 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['email']),
-) {}
+) {
+  @ApiProperty({ example: 'test4321', required: false })
+  @IsString()
+  @IsNotEmpty()
+  password?: string;
+
+  @ApiProperty({ example: 'updated user name', required: false })
+  @IsString()
+  @IsNotEmpty()
+  name?: string;
+}
