c19d0ab34d694063c0ae076aa931b5ba96db1b5a
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2025-03-06 10:58:55 +0900

    docs: Add auth swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,19 @@ import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Response } from 'express';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({
+    summary: '로그인',
+  })
+  @ApiOkResponse({
+    description: 'access_token 쿠키 설정 및 응답.',
+  })
   signIn(
     @Body() signInDto: LoginDto,
     @Res({ passthrough: true }) res: Response,
